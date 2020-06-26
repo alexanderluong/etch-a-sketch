e5432f080af2c12903d4032ecb990455d2e41bec
alexanderluong <alexander.luong@hotmail.com>
2020-06-25 23:52:51 -0700

    Implement clear all blocks

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function initPage() {
+  // TODO: clear the page first
   let container = document.querySelector(".square-container");
 
   for (let x = 0; x < 16; x++) {
@@ -11,6 +12,12 @@ function initPage() {
 
 function changeToBlack(e) {
   this.style.backgroundColor = "black";
+}
+
+// Change all the squares to white
+function clearAllBlocks() {
+  const allSquares = document.querySelectorAll(".square");
+  allSquares.forEach((square) => (square.style.backgroundColor = "white"));
 }
 
 initPage();
