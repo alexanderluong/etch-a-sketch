32d82b60dc041693a1c1440512815b892e37c067
alexanderluong <alexander.luong@hotmail.com>
2020-06-26 00:00:48 -0700

    Add removeAllSquares() function

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function initPage() {
+function initPage(blocksPerSide) {
   // TODO: clear the page first
+  removeAllSquares();
   let container = document.querySelector(".square-container");
 
-  for (let x = 0; x < 16; x++) {
+  for (let x = 0; x < blocksPerSide ** 2; x++) {
     let square = document.createElement("div");
     square.className = "square";
     container.appendChild(square);
@@ -14,10 +15,19 @@ function changeToBlack(e) {
   this.style.backgroundColor = "black";
 }
 
-// Change all the squares to white
+function removeAllSquares() {
+  let squares = document.querySelectorAll(".square");
+  if (squares.length > 0) {
+    squares.forEach((square) => square.remove());
+  }
+}
+
+// Change all the squares to white and change the number of blocks
 function clearAllBlocks() {
   const allSquares = document.querySelectorAll(".square");
   allSquares.forEach((square) => (square.style.backgroundColor = "white"));
+  let blocksPerSide = prompt("How many blocks per side?");
+  initPage(blocksPerSide);
 }
 
-initPage();
+initPage(4);
