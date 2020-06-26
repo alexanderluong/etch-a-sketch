99109fafc9cf1c28e8697d085a70e032e6815caa
alexanderluong <alexander.luong@hotmail.com>
2020-06-26 00:11:27 -0700

    Clean up css and js file

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function initPage(blocksPerSide) {
-  const gridWidth = 600;
+  const gridWidth = 700;
   removeAllSquares();
   let container = document.querySelector(".square-container");
 
   for (let x = 0; x < blocksPerSide ** 2; x++) {
     let square = document.createElement("div");
-    let dimension = Math.round(gridWidth / blocksPerSide);
+    let dimension = Math.floor(gridWidth / blocksPerSide);
     square.className = "square";
     square.style.width = `${dimension}px`;
     square.style.height = `${dimension}px`;
@@ -29,7 +29,7 @@ function removeAllSquares() {
 function clearAllBlocks() {
   const allSquares = document.querySelectorAll(".square");
   allSquares.forEach((square) => (square.style.backgroundColor = "white"));
-  let blocksPerSide = prompt("How many blocks per side?");
+  let blocksPerSide = prompt("How many blocks per side?") | 4;
   initPage(blocksPerSide);
 }
 
